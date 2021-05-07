fca7b3f5d6d44af1ef0b910a5127ca6ff8283d4a
Raghda  Muayad <therose000777@gmail.com>
2021-05-07 07:39:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your solution here!
 
-const Cards = ["Milo","Otis","Garfield"];
-function prepend (Cards) {
-    Cards.push('Odie');
-  }
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");
+prepend.unshift("Odie");
+
+removeLast.pop();
+removeFirst.shift();
